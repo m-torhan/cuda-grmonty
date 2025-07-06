6138a3983f0c4e26e7a80354d982f2e7d12171d4
Maciej Torhan <maciek97x@gmail.com>
2025-07-06 22:39:20 +0200

    [ndarray] NDArray implementation
    Signed-off-by: Maciej Torhan <maciek97x@gmail.com>

---
+++ cuda_grmonty/ndarray.hpp
@@ -0,0 +1,282 @@
+#pragma once
+
+#include <algorithm>
+#include <initializer_list>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <vector>
+
+namespace ndarray {
+
+/**
+ * N-dimensional array.
+ */
+template <typename T>
+class NDArray {
+public:
+    NDArray() : data_(nullptr), shape_({}), index_({}) {};
+    NDArray(const std::vector<unsigned int> &shape);
+    NDArray(const std::vector<unsigned int> &shape, const std::vector<T> &values);
+    NDArray(const std::initializer_list<unsigned int> &shape) : NDArray(std::vector<unsigned int>(shape)) {};
+    NDArray(const NDArray<T> &other);
+    NDArray(NDArray<T> &&other) = default;
+    ~NDArray() {};
+
+    NDArray<T> &operator=(const NDArray<T> &other);
+    NDArray<T> &operator=(NDArray<T> &&other) = default;
+    T operator=(T other);
+
+    NDArray<T> operator[](const std::vector<int> &index) const;
+
+    /**
+     * Converts zero-dimensional array to single number.
+     */
+    operator T() const;
+    /**
+     * Returns number of dimensions.
+     */
+    unsigned int ndim() const;
+    /**
+     * Returns number of elements.
+     */
+    unsigned int size() const;
+    /**
+     * Returns shape of the array.
+     */
+    std::vector<unsigned int> shape() const;
+
+    /**
+     * Reads values from input stream.
+     */
+    template <typename U>
+    friend std::istream &operator>>(std::istream &is, const NDArray<U> &array);
+
+    /**
+     * Creates array of given shape filled with zeros.
+     */
+    static NDArray<T> zeros(const std::initializer_list<unsigned int> &shape);
+    /**
+     * Creates array of given shape filled with ones.
+     */
+    static NDArray<T> ones(const std::initializer_list<unsigned int> &shape);
+
+private:
+    unsigned int flat_index(std::vector<int> index) const;
+    unsigned int flat_index(int index) const;
+
+    std::shared_ptr<T[]> data_;
+    std::vector<unsigned int> shape_;
+    std::vector<int> index_;
+};
+
+template <typename T>
+NDArray<T>::NDArray(const std::vector<unsigned int> &shape) : shape_(shape) {
+    unsigned int size = 1;
+    for (const auto &s : shape) {
+        size *= s;
+    }
+    data_ = std::shared_ptr<T[]>(new T[size]);
+    index_.resize(shape.size(), -1);
+};
+
+template <typename T>
+NDArray<T>::NDArray(const std::vector<unsigned int> &shape, const std::vector<T> &values) : shape_(shape) {
+    unsigned int size = 1;
+    for (const auto &s : shape) {
+        size *= s;
+    }
+    data_ = std::shared_ptr<T[]>(new T[size]);
+    index_.resize(shape.size(), -1);
+    if (size != values.size()) {
+        throw std::invalid_argument("Invalid number of values");
+    }
+    for (int i = 0; i < (int)size; ++i) {
+        data_[i] = values[i];
+    }
+}
+
+template <typename T>
+NDArray<T>::NDArray(const NDArray<T> &other) : shape_(other.shape()) {
+    data_ = std::shared_ptr<T[]>(new T[other.size()]);
+
+    for (int i = 0; i < (int)size(); ++i) {
+        data_[i] = other.data_[other.flat_index(i)];
+    }
+    index_.resize(other.ndim(), -1);
+}
+
+template <typename T>
+NDArray<T> &NDArray<T>::operator=(const NDArray<T> &other) {
+    if (this == &other) {
+        return *this;
+    }
+    auto this_shape = shape();
+    auto other_shape = other.shape();
+    if (data_ == nullptr) {
+        data_ = std::shared_ptr<T[]>(new T[other.size()]);
+        shape_ = other.shape();
+        index_.resize(shape_.size(), -1);
+    } else {
+        if (this_shape.size() != other_shape.size()) {
+            throw std::invalid_argument("Provided ndarrays have different number of dimensions");
+        }
+        if (!std::equal(this_shape.begin(), this_shape.end(), other_shape.begin())) {
+            throw std::invalid_argument("Provided ndarrays have different shapes");
+        }
+    }
+    for (int i = 0; i < (int)size(); ++i) {
+        data_[flat_index(i)] = other.data_[other.flat_index(i)];
+    }
+
+    return *this;
+}
+
+template <typename T>
+T NDArray<T>::operator=(T other) {
+    for (int i = 0; i < (int)size(); ++i) {
+        data_[flat_index(i)] = other;
+    }
+    return other;
+}
+
+template <typename T>
+NDArray<T> NDArray<T>::operator[](const std::vector<int> &index) const {
+    if (index.size() > ndim()) {
+        throw std::invalid_argument("Specified index has too many values");
+    }
+
+    NDArray<T> ret;
+    ret.data_ = data_;
+    ret.shape_ = shape_;
+    ret.index_.clear();
+    for (int i = 0, j = 0; i < (int)shape_.size(); ++i) {
+        if (index_[i] == -1 && j < (int)index.size()) {
+            int idx = index[j] >= 0 ? index[j] : shape_[i] + index[j];
+            if (idx > (int)shape_[i] || idx < 0) {
+                throw std::invalid_argument("Invalid index");
+            }
+            ret.index_.push_back(idx);
+            ++j;
+        } else {
+            ret.index_.push_back(index_[i]);
+        }
+    }
+
+    return ret;
+}
+
+template <typename T>
+NDArray<T>::operator T() const {
+    if (0 != ndim()) {
+        throw std::invalid_argument("Cannot convert ndarray with ndim larger than 0");
+    }
+    return data_[flat_index({})];
+}
+
+template <typename T>
+unsigned int NDArray<T>::ndim() const {
+    unsigned int ndim = 0;
+    for (int i = 0; i < (int)shape_.size(); ++i) {
+        if (index_[i] == -1) {
+            ndim += 1;
+        }
+    }
+    return ndim;
+}
+
+template <typename T>
+unsigned int NDArray<T>::size() const {
+    unsigned int size = 1;
+    for (int i = 0; i < (int)shape_.size(); ++i) {
+        if (index_[i] == -1) {
+            size *= shape_[i];
+        }
+    }
+    return size;
+}
+
+template <typename T>
+std::vector<unsigned int> NDArray<T>::shape() const {
+    std::vector<unsigned int> shape;
+    for (int i = 0; i < (int)shape_.size(); ++i) {
+        if (index_[i] == -1) {
+            shape.push_back(shape_[i]);
+        }
+    }
+    return shape;
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, const NDArray<T> &array) {
+    for (int i = 0; i < (int)array.size(); ++i) {
+        T value;
+        is >> value;
+        array.data_[array.flat_index(i)] = value;
+    }
+    return is;
+}
+
+template <typename T>
+NDArray<T> NDArray<T>::zeros(const std::initializer_list<unsigned int> &shape) {
+    NDArray<T> arr(shape);
+
+    arr = 0;
+
+    return arr;
+}
+
+template <typename T>
+NDArray<T> NDArray<T>::ones(const std::initializer_list<unsigned int> &shape) {
+    NDArray<T> arr(shape);
+
+    arr = 1;
+
+    return arr;
+}
+
+template <typename T>
+unsigned int NDArray<T>::flat_index(std::vector<int> index) const {
+    if (index.size() != ndim()) {
+        throw std::invalid_argument("Specified index has invalid size");
+    }
+
+    std::vector<unsigned int> final_index;
+    for (int i = 0, j = 0; i < (int)shape_.size(); ++i) {
+        if (index_[i] == -1) {
+            final_index.push_back(index[j] >= 0 ? index[j] : index[j] + shape_[i]);
+            ++j;
+        } else {
+            final_index.push_back(index_[i]);
+        }
+    }
+
+    unsigned int flat_index = 0;
+    unsigned int shape_prod = 1;
+    for (int i = (int)final_index.size() - 1; i >= 0; --i) {
+        flat_index += shape_prod * final_index[i];
+        shape_prod *= shape_[i];
+    }
+
+    return flat_index;
+}
+
+template <typename T>
+unsigned int NDArray<T>::flat_index(int index) const {
+    if (index < 0) {
+        index += size();
+    }
+
+    std::vector<unsigned int> array_shape = shape();
+    std::vector<int> non_flat_index;
+
+    for (int i = (int)array_shape.size() - 1; i >= 0; --i) {
+        non_flat_index.push_back(index % array_shape[i]);
+        index /= array_shape[i];
+    }
+    std::reverse(non_flat_index.begin(), non_flat_index.end());
+
+    return flat_index(non_flat_index);
+}
+
+}; /* namespace ndarray */
---
+++ cuda_grmonty/tests/ndarray_test.cpp
@@ -0,0 +1,482 @@
+/*
+ * Copyright (c) 2025 Maciej Torhan <https://github.com/m-torhan>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "cuda_grmonty/ndarray.hpp"
+
+#include <algorithm>
+#include <gtest/gtest.h>
+#include <initializer_list>
+#include <istream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+typedef std::vector<unsigned int> Shape; /* initializer_list cannot be param of parametrized test */
+typedef std::vector<int> Index;
+typedef std::tuple<Shape, Index, int> ShapeIndexValue;
+
+class NDArrayShapeFixture : public ::testing::TestWithParam<Shape> {};
+
+class NDArrayShapeIndexValueFixture : public ::testing::TestWithParam<ShapeIndexValue> {};
+
+static std::string gen_shape_str(const testing::TestParamInfo<NDArrayShapeFixture::ParamType> &info);
+
+static std::string
+gen_shape_index_value_str(const testing::TestParamInfo<NDArrayShapeIndexValueFixture::ParamType> &info);
+
+/**
+ * Tests constructor with various shapes.
+ */
+TEST_P(NDArrayShapeFixture, Construct) {
+    auto shape = GetParam();
+
+    ndarray::NDArray<int> a(shape);
+}
+
+/**
+ * Tests constructor with values.
+ */
+TEST(NDArray, InitializeWithValues) {
+    ndarray::NDArray<int> a({2, 3}, {0, 1, 2, 3, 4, 5});
+
+    ASSERT_EQ(0, (a[{0, 0}]));
+    ASSERT_EQ(1, (a[{0, 1}]));
+    ASSERT_EQ(2, (a[{0, 2}]));
+    ASSERT_EQ(3, (a[{1, 0}]));
+    ASSERT_EQ(4, (a[{1, 1}]));
+    ASSERT_EQ(5, (a[{1, 2}]));
+}
+
+/**
+ * Tests constructor with values.
+ */
+TEST(NDArray, InitializeWithValuesInvalidShape) {
+    EXPECT_THROW(({ ndarray::NDArray<int> a({2, 4}, {0, 1, 2, 3, 4, 5}); }), std::invalid_argument);
+}
+
+/**
+ * Tests copying to uninitialized array.
+ */
+TEST(NDArray, CopyToUninitializedArray) {
+    ndarray::NDArray<int> a({2, 3}, {0, 1, 2, 3, 4, 5});
+    ndarray::NDArray<int> b;
+
+    b = a;
+
+    ASSERT_EQ(0, (b[{0, 0}]));
+    ASSERT_EQ(1, (b[{0, 1}]));
+    ASSERT_EQ(2, (b[{0, 2}]));
+    ASSERT_EQ(3, (b[{1, 0}]));
+    ASSERT_EQ(4, (b[{1, 1}]));
+    ASSERT_EQ(5, (b[{1, 2}]));
+}
+
+/**
+ * Tests zero dimensional array.
+ */
+TEST(NDArray, ZeroDim) {
+    ndarray::NDArray<int> a({});
+
+    a = 0;
+
+    ASSERT_EQ(0, a);
+}
+
+/**
+ * Tests retrieving ndim of ndarray.
+ */
+TEST_P(NDArrayShapeFixture, Ndim) {
+    auto shape = GetParam();
+
+    ndarray::NDArray<int> a(shape);
+
+    ASSERT_EQ(shape.size(), a.ndim());
+}
+
+/**
+ * Tests retrieving size of ndarray.
+ */
+TEST_P(NDArrayShapeFixture, Size) {
+    auto shape = GetParam();
+    unsigned int size = 1;
+    for (auto &s : shape) {
+        size *= s;
+    }
+
+    ndarray::NDArray<int> a(shape);
+
+    ASSERT_EQ(size, a.size());
+}
+
+/**
+ * Tests retrieving shape of ndarray.
+ */
+TEST_P(NDArrayShapeFixture, Shape) {
+    auto shape = GetParam();
+
+    ndarray::NDArray<int> a(shape);
+
+    std::vector<unsigned int> shape_vec(shape);
+    std::vector<unsigned int> a_shape = a.shape();
+
+    ASSERT_EQ(shape.size(), a_shape.size());
+    for (int i = 0; i < (int)shape_vec.size(); ++i) {
+        ASSERT_EQ(shape_vec[i], a_shape[i]);
+    }
+}
+
+/* clang-format off */
+INSTANTIATE_TEST_SUITE_P(NDArray, NDArrayShapeFixture,
+                         ::testing::Values(
+                            Shape{2u, 3u},
+                            Shape{1u, 1u},
+                            Shape{},
+                            Shape{3u, 4u, 5u},
+                            Shape{7u, 4u, 3u, 6u, 2u}),
+                         gen_shape_str);
+/* clang-format on */
+
+/**
+ * Tests setting and getting single values at index.
+ */
+TEST_P(NDArrayShapeIndexValueFixture, SetGetIndex) {
+    ndarray::NDArray<int> a({2, 3});
+
+    a[{0, 0}] = 1;
+    ASSERT_EQ(1, (a[{0, 0}]));
+}
+
+/* clang-format off */
+INSTANTIATE_TEST_SUITE_P(NDArray, NDArrayShapeIndexValueFixture,
+                         ::testing::Values(
+                            ShapeIndexValue{{2u, 3u}, {1, 1}, 1},
+                            ShapeIndexValue{{2u, 3u}, {1, 1}, -2},
+                            ShapeIndexValue{{2u, 3u}, {0, 2}, 3},
+                            ShapeIndexValue{{2u, 3u}, {1, 0}, 4},
+                            ShapeIndexValue{{2u, 3u}, {1, -2}, 5},
+                            ShapeIndexValue{{2u, 3u}, {-1, 2}, 6},
+                            ShapeIndexValue{{2u, 3u, 4u}, {1, 2, 3}, -7}),
+                         gen_shape_index_value_str);
+/* clang-format on */
+
+/**
+ * Tests setting single values at index with negative values.
+ */
+TEST(NDArray, SetIndexNegative) {
+    ndarray::NDArray<int> a({2, 3});
+
+    a[{-1, -3}] = 1;
+    ASSERT_EQ(1, (a[{1, 0}]));
+}
+
+/**
+ * Tests getting single values at index with negative values.
+ */
+TEST(NDArray, GetIndexNegative) {
+    ndarray::NDArray<int> a({2, 3});
+
+    a[{1, 0}] = 1;
+    ASSERT_EQ(1, (a[{-1, -3}]));
+}
+
+/**
+ * Tests setting single values at invalid index.
+ */
+TEST(NDArray, SetIndexInvalid) {
+    ndarray::NDArray<int> a({2, 3});
+
+    EXPECT_THROW(({ a[{0, 5}] = 1; }), std::invalid_argument);
+}
+
+/**
+ * Tests getting single values at invalid index.
+ */
+TEST(NDArray, GetIndexInvalid) {
+    ndarray::NDArray<int> a({2, 3});
+
+    EXPECT_THROW(({ a[{0, 5}]; }), std::invalid_argument);
+}
+
+/**
+ * Tests getting single values at invalid index.
+ */
+TEST(NDArray, GetIndexInvalidDim) {
+    ndarray::NDArray<int> a({2, 3});
+
+    EXPECT_THROW(({ a[{0, 5, 1}]; }), std::invalid_argument);
+}
+
+/**
+ * Tests assignment.
+ */
+TEST(NDArray, Assign) {
+    ndarray::NDArray<int> a({2, 3});
+    ndarray::NDArray<int> b({2, 3});
+
+    a = 0;
+
+    b = a;
+
+    ASSERT_EQ(0, (b[{0, 0}]));
+    ASSERT_EQ(0, (b[{0, 1}]));
+    ASSERT_EQ(0, (b[{0, 2}]));
+    ASSERT_EQ(0, (b[{1, 0}]));
+    ASSERT_EQ(0, (b[{1, 1}]));
+    ASSERT_EQ(0, (b[{1, 2}]));
+}
+
+/**
+ * Tests assignment with invalid ndim.
+ */
+TEST(NDArray, AssignInvalidNdim) {
+    ndarray::NDArray<int> a({2, 3});
+    ndarray::NDArray<int> b({2, 3, 1});
+
+    a = 0;
+
+    EXPECT_THROW({ b = a; }, std::invalid_argument);
+}
+
+/**
+ * Tests assignment with invalid shape.
+ */
+TEST(NDArray, AssignInvalidShape) {
+    ndarray::NDArray<int> a({2, 3});
+    ndarray::NDArray<int> b({2, 4});
+
+    a = 0;
+
+    EXPECT_THROW({ b = a; }, std::invalid_argument);
+}
+
+/**
+ * Tests converting 0-dim array to number.
+ */
+TEST(NDArray, ConvertToNumber) {
+    ndarray::NDArray<int> a({});
+
+    a = 0;
+
+    int b = a;
+
+    ASSERT_EQ(0, b);
+}
+
+/**
+ * Tests converting array with invalid number of dimensions to number.
+ */
+TEST(NDArray, ConvertToNumberInvalidDim) {
+    ndarray::NDArray<int> a({2, 3});
+
+    a = 0;
+
+    EXPECT_THROW({ int b = a; }, std::invalid_argument);
+}
+
+/**
+ * Tests setting whole ndarray with single value.
+ */
+TEST(NDArray, SetWholeNDArrayWithSingleValue) {
+    ndarray::NDArray<int> a({2, 3});
+
+    a = 0;
+    ASSERT_EQ(0, (a[{0, 0}]));
+    ASSERT_EQ(0, (a[{0, 1}]));
+    ASSERT_EQ(0, (a[{0, 2}]));
+    ASSERT_EQ(0, (a[{1, 0}]));
+    ASSERT_EQ(0, (a[{1, 1}]));
+    ASSERT_EQ(0, (a[{1, 2}]));
+}
+
+/**
+ * Tests retrieveing subarray from an array.
+ */
+TEST(NDArray, Subarray) {
+    ndarray::NDArray<int> a({2, 3});
+
+    a[{0, 0}] = 0;
+    a[{0, 1}] = 1;
+    a[{0, 2}] = 2;
+
+    ndarray::NDArray<int> b = a[{0}];
+
+    ASSERT_EQ(3, b.size());
+    ASSERT_EQ(1, b.ndim());
+    ASSERT_EQ(3, b.shape()[0]);
+    ASSERT_EQ(0, b[{0}]);
+    ASSERT_EQ(1, b[{1}]);
+    ASSERT_EQ(2, b[{2}]);
+}
+
+/**
+ * Tests retrieveing subarray copy from an array.
+ */
+TEST(NDArray, SubarrayCopy) {
+    ndarray::NDArray<int> a({2, 3});
+
+    a[{0, 0}] = 0;
+    a[{0, 1}] = 1;
+    a[{0, 2}] = 2;
+
+    ndarray::NDArray<int> b({4, 4});
+    b[{0, 0}] = -1;
+
+    ASSERT_EQ(16, b.size());
+    ASSERT_EQ(2, b.ndim());
+
+    b = a[{0}];
+
+    ASSERT_EQ(3, b.size());
+    ASSERT_EQ(1, b.ndim());
+    ASSERT_EQ(3, b.shape()[0]);
+    ASSERT_EQ(0, b[{0}]);
+    ASSERT_EQ(1, b[{1}]);
+    ASSERT_EQ(2, b[{2}]);
+}
+
+/**
+ * Tests setting subarray.
+ */
+TEST(NDArray, SetSubarray) {
+    ndarray::NDArray<int> a({2, 3});
+    ndarray::NDArray<int> b({3});
+
+    b[{0}] = 0;
+    b[{1}] = 1;
+    b[{2}] = 2;
+
+    a[{0}] = b;
+
+    ASSERT_EQ(0, (a[{0, 0}]));
+    ASSERT_EQ(1, (a[{0, 1}]));
+    ASSERT_EQ(2, (a[{0, 2}]));
+}
+
+/**
+ * Tests setting subarray with single value.
+ */
+TEST(NDArray, SetSubarrayWithSingleValue) {
+    ndarray::NDArray<int> a({2, 3});
+
+    a[{0}] = 0;
+
+    ASSERT_EQ(0, (a[{0, 0}]));
+    ASSERT_EQ(0, (a[{0, 1}]));
+    ASSERT_EQ(0, (a[{0, 2}]));
+}
+
+/**
+ * Tests move assignment operator.
+ */
+TEST(NDArray, Move) {
+    ndarray::NDArray<int> a({2, 3});
+
+    a[{0, 0}] = 0;
+    a[{0, 1}] = 1;
+    a[{0, 2}] = 2;
+    a[{1, 0}] = 3;
+    a[{1, 1}] = 4;
+    a[{1, 2}] = 5;
+
+    ndarray::NDArray<int> b({4, 4});
+    b[{0, 0}] = -1;
+
+    b = std::move(a);
+
+    ASSERT_EQ(6, b.size());
+    ASSERT_EQ(2, b.ndim());
+    ASSERT_EQ(2, b.shape()[0]);
+    ASSERT_EQ(3, b.shape()[1]);
+    ASSERT_EQ(0, (b[{0, 0}]));
+    ASSERT_EQ(1, (b[{0, 1}]));
+    ASSERT_EQ(2, (b[{0, 2}]));
+    ASSERT_EQ(3, (b[{1, 0}]));
+    ASSERT_EQ(4, (b[{1, 1}]));
+    ASSERT_EQ(5, (b[{1, 2}]));
+}
+
+/**
+ * Tests reading data from stream.
+ */
+TEST(NDArray, ReadStream) {
+    ndarray::NDArray<int> a({2, 3});
+
+    std::stringstream s;
+    s << 0 << " " << 1 << " " << 2 << " " << 3 << " " << 4 << " " << 5;
+
+    s >> a;
+
+    ASSERT_EQ(0, (a[{0, 0}]));
+    ASSERT_EQ(1, (a[{0, 1}]));
+    ASSERT_EQ(2, (a[{0, 2}]));
+    ASSERT_EQ(3, (a[{1, 0}]));
+    ASSERT_EQ(4, (a[{1, 1}]));
+    ASSERT_EQ(5, (a[{1, 2}]));
+}
+
+/**
+ * Tests creating array filled with zeros.
+ */
+TEST(NDArray, Zeros) {
+    auto a = ndarray::NDArray<int>::zeros({2, 3});
+
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            int value = a[{i, j}];
+            ASSERT_EQ(0, value);
+        }
+    }
+}
+
+/**
+ * Tests creating array filled with ones.
+ */
+TEST(NDArray, Ones) {
+    auto a = ndarray::NDArray<int>::ones({2, 3});
+
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            int value = a[{i, j}];
+            ASSERT_EQ(1, value);
+        }
+    }
+}
+
+static std::string gen_shape_str(const testing::TestParamInfo<NDArrayShapeFixture::ParamType> &info) {
+    const Shape &shape = info.param;
+
+    std::stringstream name;
+    if (shape.size() == 0) {
+        return std::string("empty_shape");
+    }
+    std::copy(shape.begin(), shape.end(), std::ostream_iterator<unsigned int>(name, "_"));
+    name << "shape";
+    std::string name_str = name.str();
+    std::replace(name_str.begin(), name_str.end(), '-', 'm');
+    return name_str;
+}
+
+static std::string
+gen_shape_index_value_str(const testing::TestParamInfo<NDArrayShapeIndexValueFixture::ParamType> &info) {
+    const Shape &shape = std::get<0>(info.param);
+    const Index &index = std::get<1>(info.param);
+    const int &value = std::get<2>(info.param);
+
+    std::stringstream name;
+    if (shape.size() == 0) {
+        name << "empty_shape";
+    } else {
+        std::copy(shape.begin(), shape.end(), std::ostream_iterator<unsigned int>(name, "_"));
+        name << "shape_";
+    }
+    std::copy(index.begin(), index.end(), std::ostream_iterator<int>(name, "_"));
+    name << "index_";
+    name << value;
+    name << "_value";
+    std::string name_str = name.str();
+    std::replace(name_str.begin(), name_str.end(), '-', 'm');
+    return name_str;
+}
