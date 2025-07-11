94d562c970ba7d24bcaa054fd73d586e83b42106
Maciej Torhan <maciek97x@gmail.com>
2025-07-11 23:56:45 +0200

    [harm] Add hotcross initialization
    Signed-off-by: Maciej Torhan <maciek97x@gmail.com>

---
+++ cuda_grmonty/mathfn.hpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 Maciej Torhan <https://github.com/m-torhan>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#pragma once
+
+namespace mathfn {
+
+/**
+ * @brief Computes the modified Bessel function of the second kind K_n(x).
+ *
+ * @param n Non-negative integer order (n >= 0)
+ * @param x Positive real argument (x > 0)
+ * @return Value of K_n(x)
+ *
+ * @throws std::invalid_argument if x <= 0 or n < 0
+ */
+double bessel_Kn(int n, double x);
+
+}; /* namespace mathfn */
