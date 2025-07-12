28fdfe2ff0ceefb9a3abcba4fe4e9301ae4d71f3
Maciej Torhan <maciek97x@gmail.com>
2025-07-12 17:01:24 +0200

    [hotcross] Move table init to separate file
    Signed-off-by: Maciej Torhan <maciek97x@gmail.com>

--- cuda_grmonty/hotcross.hpp
+++ cuda_grmonty/hotcross.hpp
@@ -6,16 +6,13 @@
 
 #pragma once
 
+#include "cuda_grmonty/ndarray.hpp"
+
 namespace hotcross {
 
 /**
- * @brief Computes the total angle-averaged Compton scattering cross-section.
- *
- * @param w Dimensionless photon energy (w = h * nu / (m_e * c^2)).
- * @param thetae Dimensionless electron temperature (thetae = k * T_e / (m_e * c^2)).
- *
- * @return Total angle-averaged Compton scattering cross-section, in units of the Thomson cross-section (sigma_T).
+ * @brief Initializes look-up table for hot cross sections.
  */
-double total_compton_cross_num(double w, double theta_e);
+void init_table(ndarray::NDArray<double> &table);
 
 }; /* namespace hotcross */
