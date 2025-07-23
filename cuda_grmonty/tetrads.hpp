3ec2e5f6e7a5daae37038bdb691e1f4316166640
Maciej Torhan <mtorhan@antmicro.com>
2025-07-23 14:05:04 +0200

    [harm] Add super photon creator
    Signed-off-by: Maciej Torhan <maciek97x@gmail.com>

--- cuda_grmonty/tetrads.hpp
+++ cuda_grmonty/tetrads.hpp
@@ -11,6 +11,16 @@
 
 namespace tetrads {
 
+void coordinate_to_tetrad(const double (&e_cov)[consts::n_dim][consts::n_dim], const double (&k)[consts::n_dim],
+                          double (&k_tetrad)[consts::n_dim]);
+
+void tetrad_to_coordinate(const double (&e_con)[consts::n_dim][consts::n_dim], const double (&k_tetrad)[consts::n_dim],
+                          double (&k)[consts::n_dim]);
+
+void make_tetrad(const double (&u_con)[consts::n_dim], double (&trial)[consts::n_dim],
+                 const ndarray::NDArray<double> &g_cov, double (&e_con)[consts::n_dim][consts::n_dim],
+                 double (&e_cov)[consts::n_dim][consts::n_dim]);
+
 void lower(const double (&u_con)[consts::n_dim], const ndarray::NDArray<double> &g_cov, double (&u_cov)[consts::n_dim]);
 
 }; /* namespace tetrads */
