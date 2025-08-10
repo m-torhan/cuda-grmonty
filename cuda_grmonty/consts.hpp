ba20ae48e1dbe1846907a993b1f93818640c6554
Maciej Torhan <maciek97x@gmail.com>
2025-08-10 21:41:02 +0200

    [harm] Fix super photon tracking
    Signed-off-by: Maciej Torhan <maciek97x@gmail.com>

--- cuda_grmonty/consts.hpp
+++ cuda_grmonty/consts.hpp
@@ -36,21 +36,21 @@ constexpr double etol = 1.0e-3;
 constexpr int max_iter = 2;
 constexpr int max_n_step = 1280000;
 
-constexpr double ee = 4.80320680e-10;                /* electron charge */
-constexpr double cl = 2.99792458e10;                 /* speed of light */
-constexpr double me = 9.1093826e-28;                 /* electron mass */
-constexpr double mp = 1.67262171e-24;                /* proton mass */
-constexpr double mn = 1.67492728e-24;                /* neutron mass */
-constexpr double amu = 1.66053886e-24;               /* atomic mass unit */
-constexpr double hpl = 6.6260693e-27;                /* Planck constant */
-constexpr double hbar = hpl / 2. * std::numbers::pi; /* Planck's consant / 2pi */
-constexpr double kbol = 1.3806505e-16;               /* Boltzmann constant */
-constexpr double g_newt = 6.6742e-8;                 /* Gravitational constant */
-constexpr double sif = 5.670400e-5;                  /* Stefan-Boltzmann constant */
-constexpr double rgas = 8.3143e7;                    /* erg K^-1 mole^-1: ideal gas const */
-constexpr double ev = 1.60217653e-12;                /* electron volt in erg */
-constexpr double sigma_thomson = 0.665245873e-24;    /* Thomson cross section in cm^2 */
-constexpr double jy = 1.e-23;                        /* Jansky flux/freq. uni; in cgs */
+constexpr double ee = 4.80320680e-10;                  /* electron charge */
+constexpr double cl = 2.99792458e10;                   /* speed of light */
+constexpr double me = 9.1093826e-28;                   /* electron mass */
+constexpr double mp = 1.67262171e-24;                  /* proton mass */
+constexpr double mn = 1.67492728e-24;                  /* neutron mass */
+constexpr double amu = 1.66053886e-24;                 /* atomic mass unit */
+constexpr double hpl = 6.6260693e-27;                  /* Planck constant */
+constexpr double hbar = hpl / (2. * std::numbers::pi); /* Planck's consant / 2pi */
+constexpr double kbol = 1.3806505e-16;                 /* Boltzmann constant */
+constexpr double g_newt = 6.6742e-8;                   /* Gravitational constant */
+constexpr double sif = 5.670400e-5;                    /* Stefan-Boltzmann constant */
+constexpr double rgas = 8.3143e7;                      /* erg K^-1 mole^-1: ideal gas const */
+constexpr double ev = 1.60217653e-12;                  /* electron volt in erg */
+constexpr double sigma_thomson = 0.665245873e-24;      /* Thomson cross section in cm^2 */
+constexpr double jy = 1.e-23;                          /* Jansky flux/freq. uni; in cgs */
 
 constexpr double pc = 3.085678e18;      /* parsec */
 constexpr double au = 1.49597870691e13; /* Astronomical Unit */
@@ -101,5 +101,12 @@ namespace super_photon {
 constexpr double jcst = std::numbers::sqrt2 * ee * ee * ee / (27.0 * me * cl * cl);
 
 }; /* namespace super_photon */
+
+namespace spectrum {
+
+constexpr double d_l_e = 0.25;          /* bin width */
+const double l_e_0 = std::log(1.0e-12); /* location of first bin, in electron rest-mass units */
+
+}; /* namespace spectrum */
 
 }; /* namespace consts */
--- cuda_grmonty/main.cpp
+++ cuda_grmonty/main.cpp
@@ -9,6 +9,7 @@
 
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
+#include "cuda_grmonty/ndarray.hpp"
 #include "spdlog/spdlog.h"
 
 #include "cuda_grmonty/harm_model.hpp"
@@ -19,6 +20,7 @@
 ABSL_FLAG(int, photon_n, 5000000, "Estimate of photon number");
 ABSL_FLAG(double, mass_unit, 4e19, "Mass unit");
 ABSL_FLAG(std::string, harm_dump_path, "", "HARM dump file path");
+ABSL_FLAG(std::string, spectrum_path, "", "Spectrum file path");
 ABSL_FLAG(spdlog::level::level_enum, verbosity, spdlog::level::info, "Logging verbosity");
 
 int main(int argc, char *argv[]) {
@@ -27,6 +29,7 @@ int main(int argc, char *argv[]) {
     auto photon_n = absl::GetFlag(FLAGS_photon_n);
     auto mass_unit = absl::GetFlag(FLAGS_mass_unit);
     auto harm_dump_path = absl::GetFlag(FLAGS_harm_dump_path);
+    auto spectrum_path = absl::GetFlag(FLAGS_spectrum_path);
     auto verbosity = absl::GetFlag(FLAGS_verbosity);
 
     spdlog::set_level(verbosity);
@@ -35,6 +38,7 @@ int main(int argc, char *argv[]) {
     spdlog::info("\tphoton_n: {}", photon_n);
     spdlog::info("\tmass_unit: {}", mass_unit);
     spdlog::info("\tharm_dump_path: {}", harm_dump_path);
+    spdlog::info("\tspectrum_path: {}", spectrum_path);
 
     harm::HARMModel harm_model(photon_n, mass_unit);
 
@@ -43,8 +47,10 @@ int main(int argc, char *argv[]) {
     harm_model.init();
 
     monty_rand::init(123);
+    // monty_rand::init(std::chrono::system_clock::now().time_since_epoch().count());
 
     auto start = std::chrono::system_clock::now();
+    auto start_0 = start;
     int n_super_photon_created = 0;
     int n_rate = 0;
 
@@ -63,11 +69,21 @@ int main(int argc, char *argv[]) {
         std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now() - start;
         if (elapsed_seconds.count() > 1.0) {
             double rate = n_rate / elapsed_seconds.count();
-            spdlog::debug("Rate {} ph/s", rate);
+            auto [zone_x_1, zone_x_2] = harm_model.get_zone_x();
+            spdlog::info("Rate {:.2f} ph/s, zone ({}, {})", rate, zone_x_1, zone_x_2);
             n_rate = 0;
             start = std::chrono::system_clock::now();
         }
     }
+    auto stop = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = stop - start_0;
+    spdlog::info("Final rate {:.2f} ph/s", n_super_photon_created / elapsed_seconds.count());
+    spdlog::info("Super photons:");
+    spdlog::info("\tcreated: {}", n_super_photon_created);
+    spdlog::info("\tscattered: {}", harm_model.get_n_super_photon_scattered());
+    spdlog::info("\trecorded: {}", harm_model.get_n_super_photon_recorded());
+
+    harm_model.report_spectrum(n_super_photon_created, spectrum_path);
 
     return 0;
 }
