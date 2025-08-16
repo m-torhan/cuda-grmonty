9b7f5d74bb24c16827a785f8bc0f1212c790216f
Maciej Torhan <maciek97x@gmail.com>
2025-08-16 12:41:22 +0200

    [tool] Add script for spectrum plotting
    Signed-off-by: Maciej Torhan <maciek97x@gmail.com>

---
+++ plot_spectrum.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+# /// script
+# dependencies = [
+#   "aplpy",
+#   "astropy<6.0",
+#   "matplotlib",
+#   "nmmn @ git+https://github.com/rsnemmen/nmmn",
+#   "numpy",
+#   "scipy",
+#   "uncertainties",
+# ]
+# ///
+
+"""
+Simple script for spectrum plotting.
+"""
+
+import nmmn.sed
+import argparse
+from pathlib import Path
+from pylab import plot
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(__doc__)
+
+    parser.add_argument("--spectrum_path", type=Path, required=True)
+    parser.add_argument("--plot_path", type=Path, required=True)
+
+    args = parser.parse_args()
+
+    s = nmmn.sed.SED()
+    s.grmonty(str(args.spectrum_path.resolve()))
+
+    plt.figure()
+    plot(s.lognu, s.ll)
+    plt.savefig(str(args.plot_path.resolve()), dpi=600, bbox_inches="tight")
+
+    print(f"Spectrum plot saved to {args.plot_path}")
